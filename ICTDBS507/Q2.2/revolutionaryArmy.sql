e3bb63952bcb5fe86d3e46ae0506ca01934c5faa
ktnishide <ktnishide@gmail.com>
2022-06-08 23:36:44 +1000

    ICTWEB513 complete

---
+++ ICTDBS507/Q2.2/revolutionaryArmy.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `members` (
+	`id`	INTEGER AUTO_INCREMENT,
+	`name`	TEXT,
+	`epithet`	TEXT,
+	PRIMARY KEY(`id`)
+);
+INSERT INTO `members` (`id`,`name`,`epithet`) VALUES (1,'Monkey D. Dragon','Supreme Commander');
+INSERT INTO `members` (`id`,`name`,`epithet`) VALUES (2,'Sabo','Chief of Staff');
+INSERT INTO `members` (`id`,`name`,`epithet`) VALUES (3,'Emporio Ivankov','G Army Commander');
+INSERT INTO `members` (`id`,`name`,`epithet`) VALUES (4,'Karasu','North Army Commander');
+INSERT INTO `members` (`id`,`name`,`epithet`) VALUES (5,'Belo Betty','East Army Commander');
+INSERT INTO `members` (`id`,`name`,`epithet`) VALUES (6,'Morley','West Army Commander');
+INSERT INTO `members` (`id`,`name`,`epithet`) VALUES (7,'Lindbergh','South Army Commander');
+INSERT INTO `members` (`id`,`name`,`epithet`) VALUES (8,'Koala','Partner of Sabo');
+INSERT INTO `members` (`id`,`name`,`epithet`) VALUES (9,'Inazuma','G Army Member');
+INSERT INTO `members` (`id`,`name`,`epithet`) VALUES (10,'Bartholomew Kuma','World Government Plant');
+INSERT INTO `members` (`id`,`name`,`epithet`) VALUES (11,'Hack','Partner of Sabo');
